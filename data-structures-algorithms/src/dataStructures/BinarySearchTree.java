a03a9d6f97220b003cee6f5662e8fe9a576431ca
Bharati <bharatisharada@gmail.com>
2020-11-11 23:36:39 +0530

    Implemented BST recursive delete

--- data-structures-algorithms/src/dataStructures/BinarySearchTree.java
+++ data-structures-algorithms/src/dataStructures/BinarySearchTree.java
@@ -80,22 +80,84 @@ public class BinarySearchTree {
 		}
 	}
 	
+	public boolean delete(int key) {
+		if(find(root,key)) {
+			root = deleteHelper(root, key);
+			return true;
+		}
+		return false;
+	}
+	
+	private Node deleteHelper(Node root, int key) {
+		if(root==null) return null;
+		
+		if(key == root.data) {
+			// CASE 1: LEAF NODE
+			if(root.left == null && root.right==null) {
+				return null;
+			}
+			// CASE 2: LEFT SUBTREE NULL
+			else if(root.left == null) {
+				return root.right;
+			}
+			// CASE 3: RIGHT SUBTREE NULL
+			else if(root.right == null) {
+				return root.left;
+			} 
+			// CASE 4: LEFT AND RIGHT SUBTREE NOT NULL
+			int minVal = findMin(root.right);
+			System.out.println("\n"+minVal +"\n");
+			root.data = minVal;
+			root.right = deleteHelper(root.right, minVal);
+			return root;
+		}
+		
+		else if(key < root.data) {
+			root.left = deleteHelper(root.left, key);
+			return root;
+		}
+		
+		root.right = deleteHelper(root.right, key);
+		return root;
+				
+	}
+	
+	private int findMin(Node root) {
+		while(root.left!=null) {
+			root = root.left;
+		}
+		return root.data;
+	}
 	
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
-		bst.insert(10);
-        bst.recursiveInsert(-5);
-        bst.insert(-8);
-        bst.recursiveInsert(16);
+		bst.insert(7);
+        bst.recursiveInsert(5);
         bst.insert(20);
+        bst.recursiveInsert(4);
+        bst.insert(18);
         bst.recursiveInsert(25);
-        bst.insert(7);
+        bst.insert(2);
+        bst.insert(11);
+        bst.insert(19);
+        bst.insert(33);
+        bst.insert(1);
+        bst.insert(3);
+        bst.insert(14);
+        bst.insert(28);
+        bst.insert(12);
+        bst.insert(15);
+        bst.recursiveInsert(31);
+        
+        System.out.println(bst.delete(7) == false ? "Element not in the Tree\n" : "Deleted\n");
+        System.out.println(bst.delete(9) == false ? "Element not in the Tree\n" : "Deleted\n");
+
         
         System.out.println("INORDER TRAVERSAL: ");
         bst.inorderTraversal(root);
         System.out.println();
-        System.out.println(bst.find(root, 65) == false ? "Not found" : "Found");
-        System.out.println(bst.find(root, 80) == false ? "Not found" : "Found");
+        System.out.println(bst.find(root, 8) == false ? "Not found" : "Found");
+        System.out.println(bst.find(root, 10) == false ? "Not found" : "Found");
 	}
 
 }
